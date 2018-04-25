600b5c218968019c3a87fefd54760e58278f985a
jenozsolttoth <jeno_zsolt_toth@jencysoft.com>
2018-04-25 18:30:38 +0200

    Introducing ReturnState
    Introducing return state and cleaner code.

---
+++ L2Mentoring.Module1/ReturnState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L2Mentoring.Module1
+{
+    public class ReturnState
+    {
+        public static readonly ReturnState READY = new ReturnState(0, "READY");
+        public static readonly ReturnState NOPRODUCTS = new ReturnState(1, "No products in the order.");
+        public static readonly ReturnState PRODUCTNOTFOUND = new ReturnState(2,"Product not found.");
+        public static readonly ReturnState HELP = new ReturnState(3, "L2Mentorin.Module1.exe usage:\n" +
+                                                                     " \tL2Mentoring.Module1.exe \"Oleksandr Zhevzhyk\" " +
+                                                                     "\"ProductA:2;ProductB:1;ProductC:1\"");
+        public static readonly ReturnState NOPARAMETERS = new ReturnState(4, "No parameters provided. Run L2Mentoring.Module1.exe help");
+        public static readonly ReturnState ORDERWASNOTPOSTED = new ReturnState(5, "Order was not posted.");
+        
+        private ReturnState(int returnCode, string message)
+        {
+            ReturnCode = returnCode;
+            Message = message;
+        }
+        public int ReturnCode { get; }
+        public string Message { get; }
+    }
+}
